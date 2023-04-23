ec8a04024a153dc0a769843f512d29dec7b10766
Cablefish1 <37123600+Cablefish1@users.noreply.github.com>
2023-04-23 21:41:18 +0200

    Add files via upload
    Project initiated

---
+++ Survival Sim Github/PlayerData.gd
@@ -0,0 +1,14 @@
+extends Node
+
+@export_category("Skills")
+@export var skills = {} 
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
