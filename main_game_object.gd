581a5de534b63eaa8ac2f90ae56690462e942d32
Cablefish1 <erik@bzby.dk>
2023-04-24 17:33:08 +0200

    Added storyteller

--- main_game_object.gd
+++ main_game_object.gd
@@ -1,11 +1,32 @@
 extends Control
 
+@onready var storyteller = $Panel/Storyteller
+@onready var player = $PlayerDataObject
+
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	display_text("Game initiated")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	pass
+
+
+func display_text(text):
+	storyteller.add_text(text)
+	storyteller.newline()
+
+
+func display_skills():
+	for skill in player.skills:
+		var value = player.skills[skill]
+		storyteller.add_text(str(skill))
+		storyteller.add_text(str(value))
+		storyteller.newline()
+		
+
+func _on_display_sheet_pressed():
+	display_skills()
